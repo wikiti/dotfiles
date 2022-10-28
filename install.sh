668f68562a18992b15518b65d2a4cd232621b29f
Daniel Herzog <wikiti.doghound@gmail.com>
2022-10-28 22:59:21 +0100

    Enabled commented code

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ export DOTFILES_TARGET=${DOTFILES_TARGET:-$HOME/.dotfiles}
 export DOTFILES_REPO=${DOTFILES_REPO:-git@github.com:wikiti/dotfiles.git}
 
 clone $DOTFILES_REPO $DOTFILES_TARGET || exit 0
-# cd $DOTFILES_TARGET
+cd $DOTFILES_TARGET
 
 for file in install/*.sh
 do
